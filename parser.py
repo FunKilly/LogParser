eda0960276bf34a7dcb33e3912803035118fb7b4
funkill <aniol-mierci@o2.pl>
2019-07-22 23:01:13 +0200

    Deleted Lexer package, lexing file on my own

--- parser.py
+++ parser.py
@@ -1,32 +1,35 @@
-from lexer import MyLexer
 import re
 import datetime
 import argparse
-from typing import NewType
-
-dt = NewType('dt', datetime.datetime)
 
 
 class Parser(object):
-    """ Function process a return from lexer to logs statistics """
+
+    LOG_RE = re.compile(r"""
+    ^\[pid:\s  (?P<pid>\d+)  \|app:\s  (?P<app>\d+)  \|req:\s(?P<log_num>\d+)
+    \/(?P<total_num>\d+)]\s(?P<ip>\d+.\d+.\d+.\d+)\s\(\)\s{(?P<vars>\d+\svars\sin\s\d+\sbytes)}\s\[
+    (?P<datetime>.+?)\]\s(?P<method>\w+)\s.+generated\s(?P<request_size>\d+)[^(]+[^ ]+\s(?P<code_status>\d+).+
+    """, re.X)
+
     def __init__(self, filename=''):
         self.filename = filename
-        self.log_stats = []
-        self.lexer = MyLexer()
         self.date_range = {'from_date': None, 'to_date': None}
+        self.log_stats = []
+        self.args = ()
 
     def arguments_parsing(self):
+        # Initializing script arguments.
         parser = argparse.ArgumentParser()
         parser.add_argument('--from', dest='from_date',
                             help='Podaj datę od której należy rozpocząć parsowanie', required=False)
         parser.add_argument('--to', dest='to_date',
                             help='Podaj datę na której należy zakończyć parsowanie', required=False)
         parser.add_argument('filename', help="Podaj nazwe pliku z logami")
-        args = parser.parse_args()
-        self.convert_date(args.from_date, args.to_date)
-        return args.filename
+        self.args = parser.parse_args()
+        pass
 
     def convert_date(self, from_date, to_date):
+        # Converting date objects from input to datetime type or signalizing wrong input format.
         if from_date:
             try:
                 from_date = datetime.datetime.strptime(from_date, '%d-%m-%Y_%H-%M-%S')
@@ -42,77 +45,77 @@ class Parser(object):
                 print("Zły format daty: '", to_date, "' Poprawny format: %d-%m-%Y_%H-%M-%S")
         pass
 
-    def parse_range(self):
-        '''for log in self.log_stats:
-            if self.date_range['from_date'] and log[1] < self.date_range['from_date']:
-                print(log)
-            if self.date_range['to_date'] and log[1] > self.date_range['to_date']:
-                print(self.date_range['to_date'])
-                self.log_stats.pop(self.log_stats.index(log))'''
+    def parse_range(self, log_stats):
+        # Setting date range for parsing, if date isn't transferred, the date from logs is taken.
         if self.date_range['from_date'] is None:
-            self.date_range['from_date'] = self.log_stats[0][1]
-        elif self.date_range['to_date'] is None:
-            self.date_range['to_date'] = self.log_stats[-1][1]
-        self.log_stats = [log for log in self.log_stats if self.date_range['from_date'] <= log[1] <= self.date_range['to_date']]
-        return self.log_stats
+            self.date_range['from_date'] = log_stats[0][1]
+        if self.date_range['to_date'] is None:
+            self.date_range['to_date'] = log_stats[-1][1]
+        pass
 
-
-    @staticmethod
-    def read(filename):
-        # Read a log file.
+    def read(self, filename):
+        # Reading logfile line by line, transfering needed data from it with correct type.
         with open(filename, 'r') as logfile:
-            logs = logfile.readlines()
-        return logs
+            log = logfile.readline()
+            while log:
+                if re.match(r'^\[pid', log):
+                    log = self.LOG_RE.search(log).groupdict()
+                    log['datetime'] = datetime.datetime.strptime(log['datetime'], '%c')
+                    log['request_size'] = int(log['request_size'])
+                    log_data = [log['log_num'], log['datetime'], log['code_status'], log['request_size']]
+                    yield log_data
+                log = logfile.readline()
+
+    def generate_logs(self):
+        # Generating correct list with statistics for each log for defined date range.
+        log_stats = [log for log in self.read(self.args.filename)]
+        self.parse_range(log_stats)
+        log_stats = [log for log in log_stats
+                     if self.date_range['from_date'] <= log[1] <= self.date_range['to_date']]
+        self.log_stats = log_stats
+        pass
 
     @staticmethod
-    def group_tokens(tokens: list, n: int):
-        # Group tokens from 1 request.
-        for i in range(0, len(tokens), n):
-            val = tokens[i:i + n]
-            if len(val) == n:
-                yield tuple(val)
-
-    @staticmethod
-    def calc_rps(start_date: dt, end_date: dt, numbofreq: int):
+    def calc_rps(start_date, end_date, numbofreq):
         # Calculate requests per seconds.
         difference = (end_date - start_date)
         rps = int(difference.total_seconds())/numbofreq
         return f'Zapytania/sec: {round(rps, 2)}'
 
     @staticmethod
-    def group_codes(log_stats: list):
-        # Group codes by size and counter them.
+    def group_codes(log_stats):
+        # Group codes by code and countering their average size.
         codes_size = {}
         codes_counter = {}
 
         for request in log_stats:
-            if request[3] not in codes_size.keys():
-                codes_size[request[3]] = request[2]
+            if request[2] not in codes_size.keys():
+                codes_size[request[2]] = request[3]
             else:
-                codes_size[request[3]] += request[2]
-            if request[3] not in codes_counter.keys():
-                codes_counter[request[3]] = 1
+                codes_size[request[2]] += request[3]
+            if request[2] not in codes_counter.keys():
+                codes_counter[request[2]] = 1
             else:
-                codes_counter[request[3]] += 1
+                codes_counter[request[2]] += 1
 
         return codes_size, codes_counter
 
     def generate_response(self):
-        # Generate response for script.
 
-        # Generate number of requests
+        # Generate number of requests.
         print(f'Zapytan: {len(self.log_stats)}')
 
-        # Generate number of request per second
+        # Generate number of request per second.
         print(self.calc_rps(self.log_stats[0][1], self.log_stats[-1][1], len(self.log_stats)))
 
         codes_size, codes_counter = self.group_codes(self.log_stats)
-        # Combine request for a code counter
+
+        # Combine request for a code counter.
         response_code_size = ''
         response_code_counter = ', '.join(f'{key}: {value}' for key, value in codes_counter.items())
         response_code_counter = f'Odpowiedzi: ({response_code_counter})'
 
-        # Combine request for a average code size
+        # Combine request for a average code size.
         for code, size in codes_size.items():
             unit = 'Mb'
             size_rounded = round(((size / codes_counter[code]) / (1024 * 1024)), 2)
@@ -128,17 +131,14 @@ class Parser(object):
         print(response_code_size)
 
     def main(self):
-        # Reading a file, than creating lexer instance with input.
-        for line in self.read(self.arguments_parsing()):
-            if re.match(r"\[\D{3}:\s\d+", line):
-                self.lexer.build()
-                self.lexer.run(line)
-        # Process of return from lexer
-        self.log_stats = [item for item in self.group_tokens(self.lexer.log_stat, 4)]
-        if self.date_range:
-            self.parse_range()
 
-        self.generate_response()
+        self.arguments_parsing()
+        self.convert_date(self.args.from_date, self.args.to_date)
+        self.generate_logs()
+        try:
+            self.generate_response()
+        except IndexError:
+            print(f'Podany przedział czasowy zwrócił {len(self.log_stats)} rekordów. Brak statystyk.')
 
 
 if __name__ == '__main__':
